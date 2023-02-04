3bb37060e04a5d13c7abf0d5c34b58760287faa6
TarkanV <carchaido@gmail.com>
2023-02-04 17:13:16 +0100

    Add coloring function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gridNode = document.querySelector(".grid-container");
-
+const body = document.querySelector("body");
 //let rowSize = 8;
 let colSize = 16;
 let gridSize = colSize * colSize;
@@ -14,8 +14,39 @@ for(let i = 0; i < gridSize; i++){
  //boxs[i].textContent = "O";
  gridNode.appendChild(boxs[i]);
 }
+let isDown = null;
 
-boxs.forEach(box => {
-    box.addEventListener("mouseover", () => box.classList.toggle("hover",true));
-    box.addEventListener("mouseout", ()=> box.classList.toggle("hover", false));
+body.addEventListener("mouseover", (e) => {
+    e.stopPropagation();   
+   if(!e.target.classList.contains("box")) isDown = false;
 });
+
+    boxs.forEach(box => {
+        
+        //box.setAttribute('draggable', false);
+        
+        //#1 Have to change background-color value to a variable
+
+        //Colors whe
+        box.addEventListener("mousedown", () => {
+            box.style.backgroundColor = "black"; 
+            isDown = true;
+            console.log("isDown");
+        });
+
+        //Adds hover effect to box the cursor is on and colors it if mousedown
+        box.addEventListener("mousemove", (e) => {
+            box.classList.toggle("hover",true);
+            if(isDown) box.style.backgroundColor = "black";
+        });
+
+        //Remove hover effect from boxes the cursor left 
+        box.addEventListener("mouseout", ()=> {box.classList.toggle("hover", false);
+        }); 
+
+        //Cancels hover and mousedown coloring
+        box.addEventListener("mouseup", () => {
+            isDown = false;
+        });
+        
+    });
