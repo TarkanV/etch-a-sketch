c4351ad00fa6ee6c67b31be9b5c7c53c63db7b36
TarkanV <carchaido@gmail.com>
2023-02-04 18:42:56 +0100

    Fix mouseover and mousedown event combination bug

--- script.js
+++ script.js
@@ -1,52 +1,88 @@
+//#1 Have to change background-color value to a variable
+
 const gridNode = document.querySelector(".grid-container");
 const body = document.querySelector("body");
+const reset = document.querySelector(".reset");
+
+let boxs = null;
 //let rowSize = 8;
 let colSize = 16;
-let gridSize = colSize * colSize;
-gridNode.style.gridTemplateColumns = `repeat(${colSize}, 1fr)`;
+reset.addEventListener("click", () => initializeGrid(colSize));
 
-let boxs = [];
-for(let i = 0; i < gridSize; i++){
- boxs[i] = document.createElement("div");
- boxs[i].dataset.pos = i.toString();
- boxs[i].classList.add("box");
+function initializeGrid(colSize){
+    console.log("this is called");
 
- //boxs[i].textContent = "O";
- gridNode.appendChild(boxs[i]);
+    if (boxs != null){
+        boxs.forEach(box =>{
+            gridNode.removeChild(box);
+            
+        });     
+    }
+    
+    boxs = [];
+    let gridSize = colSize * colSize;
+    gridNode.style.gridTemplateColumns = `repeat(${colSize}, 1fr)`;
+    
+    for(let i = 0; i < gridSize; i++){
+        boxs[i] = document.createElement("div");
+        boxs[i].dataset.pos = i.toString();
+        boxs[i].classList.add("box");
+        gridNode.appendChild(boxs[i]);
+        //boxs[i].textContent = "O";
 }
-let isDown = null;
+    
+    addColoringEvents();
 
-body.addEventListener("mouseover", (e) => {
-    e.stopPropagation();   
-   if(!e.target.classList.contains("box")) isDown = false;
-});
+}
 
-    boxs.forEach(box => {
-        
-        //box.setAttribute('draggable', false);
-        
-        //#1 Have to change background-color value to a variable
 
-        //Colors whe
+function addColoringEvents(){
+    let isDown = null;
+
+    
+    
+    
+
+    /*
+    gridNode.addEventListener("mouseup", () => {
+        isDown = false;
+    });
+    gridNode.addEventListener("mousedown", () => { 
+        isDown = true;
+    });
+    gridNode.addEventListener("mouseleave", (e) => {
+        isDown = false;
+    });
+    */
+
+
+
+    boxs.forEach(box => {     
+        //Colors when left mouse button is clicked
         box.addEventListener("mousedown", () => {
             box.style.backgroundColor = "black"; 
-            isDown = true;
-            console.log("isDown");
         });
 
         //Adds hover effect to box the cursor is on and colors it if mousedown
-        box.addEventListener("mousemove", (e) => {
+        box.addEventListener("mouseover", (e) => {
             box.classList.toggle("hover",true);
-            if(isDown) box.style.backgroundColor = "black";
+            if(e.buttons > 0) box.style.backgroundColor = "black";
         });
-
+       
         //Remove hover effect from boxes the cursor left 
-        box.addEventListener("mouseout", ()=> {box.classList.toggle("hover", false);
+        box.addEventListener("mouseout", ()=> {
+            box.classList.toggle("hover", false);
         }); 
 
         //Cancels hover and mousedown coloring
-        box.addEventListener("mouseup", () => {
-            isDown = false;
-        });
+     
         
     });
+}
+
+initializeGrid(colSize);
+
+
+
+
+
